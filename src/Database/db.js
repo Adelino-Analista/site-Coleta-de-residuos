e7aaada80d22e8290b02aec2c2ce4941f645d916
Antonio Adelino <62516426+Adelino-Analista@users.noreply.github.com>
2020-06-09 13:23:39 -0300

    Banco de dados
    corrigir erro no Banco de dados

--- src/Database/db.js
+++ src/Database/db.js
@@ -11,68 +11,67 @@ db.serialize(() => {
    // com commandos SQL eu vou
 
    // 1 criar tabela
-/* db.run(`
-       CREATE TABLE IF NOT EXISTS places (
-         id INTEGER PRIMARY KEY AUTOINCREMENT,
-         image TEXT,
-         name TEXT,
-         address TEXT,
-         address2 TEXT,
-         state TEXT,
-         city TEXT,
-         items TEXT
-       );
-    `)
-
- // 2 inserir dados da tabela
- const query = `
-INSERT INTO places (
- image,
- name,
- address,
- address2,
- state,
- city,
- items
- ) VALUES (?, ?, ?, ?, ?, ?, ?);
-`
- const values = [
-    "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80",
-    "Colectoria",
-    "Guilherme Gemballa, jardim America",
-    "Nº 260",
-    "Santa Catarina",
-    "Rio do Sul",
-    "Residous Eletronicos, Lampadas"
- ]
-
- //function afterInsertData(err) {
- // if (err) {
- //     return console.log(err)
-
-
- //console.log("Cadastro com Sucesso")
- //console.log(this)
- //}
-
- //db.run(query, values, afterInsertData) * /
-
- //db.run(query, values, afterInsertData)
-
- // 3 consulta os dados da tabela
- // db.all(`SELECT * FROM places WHERE id = ? `, [1], function (err, rows) {
- //if (err) {
- // return console.log(err)
- // }
- // console.log.log("Registro deletado co scesso!")
- //})
- // 4 deletar  dado da tabela
- //db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
- // if (err) {
- //  return console.log(err)
- // }
- //console.log("Registro deletado com Sucesso!")
- // })
+   /* db.run(`
+          CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+          );
+       `)
+   
+    // 2 inserir dados da tabela
+    const query = `
+   INSERT INTO places (
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city,
+    items
+    ) VALUES (?, ?, ?, ?, ?, ?, ?);
+   `
+    const values = [
+       "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80",
+       "Colectoria",
+       "Guilherme Gemballa, jardim America",
+       "Nº 260",
+       "Santa Catarina",
+       "Rio do Sul",
+       "Residous Eletronicos, Lampadas"
+    ]
+   
+    //function afterInsertData(err) {
+    // if (err) {
+    //     return console.log(err)
+   
+   
+    //console.log("Cadastro com Sucesso")
+    //console.log(this)
+    //}
+   
+    //db.run(query, values, afterInsertData) * /
+   
+    //db.run(query, values, afterInsertData)
+   
+    // 3 consulta os dados da tabela
+    // db.all(`SELECT * FROM places WHERE id = ? `, [1], function (err, rows) {
+    //if (err) {
+    // return console.log(err)
+    // }
+    // console.log.log("Registro deletado co scesso!")
+    //})
+    // 4 deletar  dado da tabela
+    //db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
+    // if (err) {
+    //  return console.log(err)
+    // }
+    //console.log("Registro deletado com Sucesso!")
+    // })*/
 
 })
-
